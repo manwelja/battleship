e341c01ce7b55b393d8ddf8424db77da42044001
Jennifer Manwell <manwell.fortier@gmail.com>
2022-08-05 11:09:24 -0400

    Add square_colors state

--- src/components/BoardSquare.js
+++ src/components/BoardSquare.js
@@ -7,42 +7,13 @@ import { useDrop } from 'react-dnd'
 
 // Component that invokes a WorkorderListItem child for each workorder in the data set
 export default function BoardSquare(props) {
- 
-  //const [shipToDrop, setShipToDrop] = useState([""]);
-  let shipToDrop = "";
-  
-  function dropShipHere(x, y, item) {
-    console.log("ships", ALL_SHIPS)
-    ALL_SHIPS.forEach((ship) => {
-      if(ship.name === item.shipType) {
-        ship.location.direction = "horizontal";
-        for(let i = 0; i < ship.length; i++) {
-          ship.location.squares.push({x, i});
-          ship.placed = true;
-          SQUARE_COLORS[parseInt(x.toString() + i.toString())] = ship.color;
-        }
-      }
-    })
-    console.log(item.shipType)
-    console.log(SQUARE_COLORS)
-    console.log(x, y)
-  }
 
-  // function highlightSquares(x, y, item) {
-  //   console.log(item.shipType)
-  //   setShipToDrop(item.shipType)
-  // }
-  const highlightSquares = ((x, y, item) => {
-   // console.log(item.shipType)
-    shipToDrop = item.shipType
-    return true;
-  })
-
-   const [{ isOver, canDrop }, drop] = useDrop(
+  const [{ isOver, canDrop }, drop] = useDrop(
     () => ({
       accept: [ItemTypes.BATTLESHIP, ItemTypes.CARRIER, ItemTypes.CRUISER, ItemTypes.SUBMARINE, ItemTypes.DESTROYER],
-      drop: (item) => dropShipHere(props.x, props.y, item),
-      canDrop: (item) => highlightSquares(props.x, props.y, item),
+      //drop: (item) => dropShipHere(props.x, props.y, item),
+      drop: (item) => props.onShipDrop(props.x, props.y, item),
+      //canDrop: (item) => highlightSquares(props.x, props.y, item),
       //isOver: (item) => highlightSquares(props.x, props.y, item),
       collect: (monitor) => ({
         isOver: !!monitor.isOver(),
@@ -58,9 +29,9 @@ export default function BoardSquare(props) {
         className="board-square"
         key={"tile-" + props.x + props.y}
         x={props.x}
-        y={props.y}              
+        y={props.y}                          
         > 
-        {isOver && <Overlay color="black" ship={shipToDrop}/>}    
+        {isOver && <Overlay color="black"/>}    
       </div>    
   );
 
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import { React, useState, useEffect, Fragment } from "react";
 import "./GameBoard.css";
 import BoardSquare from "./BoardSquare"
+import classNames from 'classnames'
 import { ItemTypes, ALL_SHIPS, SQUARE_COLORS} from '../constants'
 import { useDrop } from 'react-dnd'
 
@@ -8,22 +9,46 @@ import { useDrop } from 'react-dnd'
 export default function GameBoard(props) {
   const numSquares = 100;
 
+  const [square_colors, setSquareColors] = useState([]);
+
   function handleSquareClick(x, y) {
      console.log(x, y);
   }
 
-  function handleShipDrop(x, y, item) {
-    console.log("handle Drop");
+  function onShipDrop(x, y, item) {
+    ALL_SHIPS.forEach((ship) => {
+      if(ship.name === item.shipType) {
+        ship.location.direction = "horizontal";
+        for(let i = 0; i < ship.length; i++) {
+          ship.location.squares.push({'x': y, 'y': i});
+          ship.placed = true;
+          SQUARE_COLORS[parseInt(y.toString() + i.toString())] = ship.color;
+          setSquareColors(SQUARE_COLORS);
+        }
+      }
+    })
  }
 
-
+ useEffect(() => {        
+  console.log("update squares")
+  console.log("state", square_colors)
+ },[square_colors])
+ 
   function renderSquare(i) {
     const x = i % 10
     const y = Math.floor(i / 10)
 
+    const boardTilesClass = classNames("gameboard-square", {
+      "green": SQUARE_COLORS[i] === "green",
+      "blue": SQUARE_COLORS[i] === "blue",
+      "yellow": SQUARE_COLORS[i] === "yellow",
+      "purple": SQUARE_COLORS[i] === "purple",
+      "beige": SQUARE_COLORS[i] === "beige"
+   }); 
+console.log(boardTilesClass)
     return (
-      <div className="gameboard-square" key={i} onClick={() => handleSquareClick(x, y)}>
-        <BoardSquare className="gameboard-square" x={x} y={y} color={SQUARE_COLORS[i]}/>
+      <div className={boardTilesClass} key={i} onClick={() => handleSquareClick(x, y)}>
+        <BoardSquare className={boardTilesClass} x={x} y={y} onShipDrop={onShipDrop}/>
       </div>
     )
   }
